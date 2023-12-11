144e8162b8812848677eb7477596a4f6b8001588
Derek Mercedes <mercedes.derek@gmail.com>
2023-12-11 13:18:01 -0700

    another fizzbuzz

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i = 10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squares = []
+    for i in int_list:
+        squares.append(i**2)
+        print(squares)
+    return squares
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    i = 1
+    while i <= 100:
+        out = ""
+        if i % 3 == 0:
+            out += "Fizz"
+        if i % 5 == 0:
+            out += "Buzz"
+        if out == "":
+            out = i
+        print(out)
+        i += 1
